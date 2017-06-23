e002b786becb6e4ff734452d6be663c4cd25084a
jero98772 <jero98772@gmail.com>
2017-06-22 21:37:29 -0500

    Update python.py

--- python.py
+++ python.py
@@ -1,14 +1,14 @@
 while True: 
     print ("optiones:")
     print ("enter'mas' para sumar 2 numeros")
-    print ("enter'menos ' para 2 numeros")
+    print ("enter'menos' para 2 numeros")
     print ("enter'multiplicar' para 2 numeros")
     print ("enter'dividir' para 2 numeros")
-    print ("enter'quitar 'el programa")
+    print ("enter'quitar'el programa")
     user_input = input(": ")
     
 if user_input =="quitar":
- break
+    break
 elif user_input == "mas":
  numero1 = float(input("entre un numero"))
  numero2 = float(input("entre otro numero"))
